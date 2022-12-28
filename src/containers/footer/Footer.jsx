0d1dca4072770dd5a4581b2e6f6406deb1cb5d88
justintsugranes <justintsugranes@proton.me>
2022-12-28 12:30:09 -0500

    add footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,6 +1,45 @@
 import './footer.css'
+import { logo } from '../../assets'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step into the future?</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={logo} alt='logo' />
+          <p>2022 misfitDodo Media. All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>555 Laurel Canyon Drive, Los Angeles, CA</p>
+          <p>555-555-5555</p>
+          <p>info@email.com</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>2022 misfitDodo Media. All rights reserved</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
