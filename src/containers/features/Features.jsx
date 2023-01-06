f783e0b70bb46377dd8e68aeb6789bddb121fabe
justintsugranes <justintsugranes@proton.me>
2023-01-06 10:44:09 -0500

    add comments

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,14 +1,24 @@
+// Import the `Feature` component and the styles for the `Features` component
 import { Feature } from '../../components'
 import './features.css'
+
+// Import the data for the heading and the features
 import { featuresData, featuresDataHeading } from '../../constants/data'
 
+// This is a functional component that doesn't receive any props
+// It returns the JSX for rendering the features section
 const Features = () => (
-  <div className='gpt3__features section__padding' id='features'>
-    <div className='gpt3__features-heading'>
-      <h1 className='gradient__text'>{featuresDataHeading.h1}</h1>
+  // {/* Render the features container */}
+  <div className="gpt3__features section__padding" id="features">
+    {/* Render the heading for the features section */}
+    <div className="gpt3__features-heading">
+      {/* Render the heading text in a gradient style */}
+      <h1 className="gradient__text">{featuresDataHeading.h1}</h1>
+      {/* Render the paragraph text */}
       <p>{featuresDataHeading.p}</p>
     </div>
-    <div className='gpt3__features-container'>
+    {/* Render the container for the features */}
+    <div className="gpt3__features-container">
       {featuresData.map((feature, index) => (
         <Feature
           key={feature.title + index}
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,14 +1,23 @@
+// Import the `Feature` component and the styles for the `WhatGPT3` component
 import { Feature } from '../../components'
 import './whatGPT3.css'
+
+// Import the data for the heading and the features
 import { whatGPT3, whatGPT3Heading } from '../../constants/data'
 
+// This is a functional component that doesn't receive any props
+// It returns the JSX for rendering the "What is GPT3?" section
 const WhatGPT3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section__margin ' id='wgpt3'>
-      <div className='gpt3__whatgpt3-feature'>
+    // Render the "What is GPT3?" container
+    <div className="gpt3__whatgpt3 section__margin " id="wgpt3">
+      {/* Render the feature for the "What is GPT3?" section */}
+      <div className="gpt3__whatgpt3-feature">
+        {/* Render a `Feature` component for the first element in the `whatGPT3` array */}
         {whatGPT3
           .filter((feature) => feature.id === 'whatgpt3')
           .map((feature, index) => (
+            // Pass the title and text for the feature as props to the `Feature` component
             <Feature
               key={feature.id + index}
               title={feature.title}
@@ -16,12 +25,18 @@ const WhatGPT3 = () => {
             />
           ))}
       </div>
-      <div className='gpt3__whatgpt3-heading'>
-        <h1 className='gradient__text'>{whatGPT3Heading.h1}</h1>
+      {/* Render the heading for the "What is GPT3?" section */}
+      <div className="gpt3__whatgpt3-heading">
+        {/* Render the heading text in a gradient style */}
+        <h1 className="gradient__text">{whatGPT3Heading.h1}</h1>
+        {/* Render the paragraph text */}
         <p>{whatGPT3Heading.p}</p>
       </div>
-      <div className='gpt3__whatgpt3-container'>
+      {/* Render the container for the additional features in the "What is GPT3?" section */}
+      <div className="gpt3__whatgpt3-container">
+        {/* Render a `Feature` component for each element in the `whatGPT3` array except for the first element */}
         {whatGPT3.slice(1).map((feature) => (
+          // Pass the title and text for the feature as props to the `Feature` component
           <Feature key={feature.id} title={feature.title} text={feature.text} />
         ))}
       </div>
